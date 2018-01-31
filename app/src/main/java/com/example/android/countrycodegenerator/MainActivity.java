56d307ddde2faedbb77793f064ac72bf9480803d
yedhink <yedhin1998@gmail.com>
2018-01-31 19:44:32 +0530

    Case Sensitivity updated

--- app/src/main/java/com/example/android/countrycodegenerator/MainActivity.java
+++ app/src/main/java/com/example/android/countrycodegenerator/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.android.countrycodegenerator;
 
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,8 +16,8 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     EditText code;
     TextView result;
-    Map<String, String> countries = new HashMap<>();
-    Map<String, String> isos = new HashMap<>();
+    Map<String, String> countries = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+    Map<String, String> isos = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +27,35 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
     }
 
-    private void createCountryMap(){
+    //Creates TreeMap for Key as ISO code and Value as Country name
+    private void createCountryMap() {
         Locale locale = new Locale("en", "US", "WIN");
         for (String iso : Locale.getISOCountries()) {
             Locale l = new Locale("", iso);
-            countries.put( iso, l.getDisplayCountry() );
+            countries.put(iso, l.getDisplayCountry());
         }
     }
 
-    private void createIsoMap(){
+    //Creates TreeMap for Key as Country name and Value as ISO code
+    private void createIsoMap() {
         for (String iso : Locale.getISOCountries()) {
             isos.put(new Locale("", iso).getDisplayCountry(), iso.toUpperCase());
         }
     }
 
-    public void iso(View V){
+    //Displays output when button is clicked
+    public void iso(View V) {
         String cd1 = code.getText().toString();
-        if(cd1.length() > 2) {
-            cd1 = Character.toUpperCase(cd1.charAt(0)) + cd1.substring(1).toLowerCase();
+        if (cd1.length() > 2) {
+            //cd1 = Character.toUpperCase(cd1.charAt(0)) + cd1.substring(1).toLowerCase();
             createIsoMap();
             result.setText(isos.get(cd1));
-        }
-        else {
+        } else {
             cd1 = cd1.toUpperCase();
             createCountryMap();
             result.setText(countries.get(cd1));
         }
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
